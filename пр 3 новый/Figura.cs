89ddecf50848484fb4d72217caf138827b44ad59
GHOST <luchshevdanil005@gmail.com>
2024-04-26 19:36:11 +0300

    risynok5

---
+++ пр 3 новый/Figura.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace пр_3_новый
+{
+    public class Figura
+    {
+        int x;
+        int y;
+        int h;
+        PictureBox pb;
+        int dx = 10;
+
+        public Figura(int nx, int ny, int nh, PictureBox npb)
+        {
+            x = nx;
+            y = ny;
+            h = nh;
+            pb = npb;
+            Draw();
+        }
+        public void Draw()
+        {
+            
+                Graphics gr = Graphics.FromImage(pb.Image);
+                SolidBrush solidBrush = new SolidBrush(Color.LightBlue);
+                gr.FillRectangle(solidBrush, x, y, h, h);
+
+                Point[] triangle1Points = { new Point(x, y), new Point(x + h / 2, y + h / 2), new Point(x, y + h) };
+                Point[] triangle2Points = { new Point(x + h, y), new Point(x + h / 2, y + h / 2), new Point(x + h, y + h) };
+                Point[] triangle3Points = { new Point(x, y), new Point(x + h / 2, y + h / 2), new Point(x + h, y) };
+                Point[] triangle4Points = { new Point(x, y + h), new Point(x + h / 2, y + h / 2), new Point(x + h, y + h) };
+
+
+
+                Pen redPen = new Pen(Color.Red, 2);
+                gr.DrawPolygon(redPen, triangle1Points);
+                gr.DrawPolygon(redPen, triangle2Points);
+                gr.DrawPolygon(redPen, triangle3Points);
+                gr.DrawPolygon(redPen, triangle4Points);
+
+
+                SolidBrush greenBrush = new SolidBrush(Color.Green);
+
+                int circleDiameter = h / 6;
+                Point circleCenter = new Point(x + h / 3 + circleDiameter, y + h / 4);
+                gr.FillEllipse(greenBrush, circleCenter.X - circleDiameter / 2, circleCenter.Y - circleDiameter / 2, circleDiameter, circleDiameter);
+                Point circleCenter2 = new Point(x + h / 3 + circleDiameter, y + 3 * h / 4);
+                gr.FillEllipse(greenBrush, circleCenter2.X - circleDiameter / 2, circleCenter2.Y - circleDiameter / 2, circleDiameter, circleDiameter);
+
+                pb.Refresh();
+
+            
+
+        }
+
+        public void Move()
+        {
+            Clear();
+            if (x + dx < 0 || x + dx > pb.Width - h)
+            {
+                dx = -dx;
+            }
+            x = x + dx;
+            Draw();
+        }
+        public void Clear()
+        {
+            Graphics gr = Graphics.FromImage(pb.Image);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, x, y, h, h);
+            pb.Refresh();
+
+        }
+
+        internal void SetPosition(int newX, int newY)
+        {
+            x = newX;
+            y = newY;
+            Draw();
+        }
+
+        internal void SetHeight(int newHeight)
+        {
+            h = newHeight;
+            Draw();
+        }
+    }
+}
+
+     
---
+++ пр 3 новый/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace пр_3_новый
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap br = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = br;
+            Graphics gr = Graphics.FromImage(br);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Refresh();
+
+        }
+
+        public void DrawFigures()
+        {
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Refresh();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                Figura fg1 = listBox1.Items[i] as Figura;
+                fg1.Draw();
+            }
+            pictureBox1.Refresh();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                int x = Convert.ToInt32(f.textBox1.Text);
+                int y = Convert.ToInt32(f.textBox2.Text);
+                int h = Convert.ToInt32(f.textBox3.Text);
+                Figura fg = new Figura(x, y, h, pictureBox1);
+                listBox1.Items.Add(fg);
+                DrawFigures();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Объект для удаления не выбран");
+            }
+            else
+            {
+                Figura selectedFigura = (Figura)listBox1.SelectedItem;
+                listBox1.Items.Remove(selectedFigura);
+                pictureBox1.Refresh();
+            }
+
+
+
+        }
+        private void UpdateButtonStates()
+        {
+            button1.Enabled = listBox1.Items.Count > 0;
+            button2.Enabled = listBox1.SelectedIndex != -1;
+            button3.Enabled = listBox1.SelectedIndex != -1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                Figura fg1 = listBox1.Items[i] as Figura;
+                fg1.Move();
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                int index = listBox1.SelectedIndex;
+                if (index != -1)
+                {
+                    int x = Convert.ToInt32(f.textBox1.Text);
+                    int y = Convert.ToInt32(f.textBox2.Text);
+                    int h = Convert.ToInt32(f.textBox3.Text);
+                    Figura selectedFigure = (Figura)listBox1.Items[index];
+                    selectedFigure.SetPosition(x, y);
+                    selectedFigure.SetHeight(h);
+                    DrawFigures();
+                }
+                else
+                {
+                    MessageBox.Show("Выберите фигуру для редактирования.");
+                }
+
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+        
+                    
+  
+
+
+
+
